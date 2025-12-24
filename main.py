e3deb1e5effa97c5fc5e975a8c58c8989844f23c
dragonish <no.web.developer@outlook.com>
2025-12-24 21:36:47 +0800

    build: 更新脚本

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import argparse
 from log import Logger
 
 
-def handler(path, is_new, is_convert, xlog):
+def handler(path: str, is_new: bool, is_convert: bool, xlog: Logger):
     if os.path.exists(path):
         xlog.info("正在遍历目录: " + path)
         dir_ignore_file = os.path.join(path, "dir_ignore.json")
@@ -25,7 +25,9 @@ def handler(path, is_new, is_convert, xlog):
             each_full_name = os.path.join(path, each_name)
             if os.path.isdir(each_full_name):
                 # 目录
-                if not is_empty_dir(each_full_name) and not (each_name in dir_ignore_list):
+                if not is_empty_dir(each_full_name) and not (
+                    each_name in dir_ignore_list
+                ):
                     res_dict["dir"].append(each_name)
                     handler(each_full_name, is_new, is_convert, xlog)
             else:
@@ -43,30 +45,33 @@ def handler(path, is_new, is_convert, xlog):
                             with io.open(each_full_name, mode="rb") as f:
                                 data = f.read()
                                 source_encoding = chardet.detect(data)
-                                if source_encoding["encoding"] != "utf-8":
+                                se = source_encoding["encoding"]
+                                if se != "utf-8":
                                     xlog.info("转换文件编码格式: " + each_full_name)
-                                    if (source_encoding["encoding"] == "GB2312"):
+                                    if se == "GB2312":
                                         content = data.decode("GBK")
-                                        content = content.encode(
-                                            "utf-8").strip()
-                                        codecs.open(each_full_name,
-                                                    'wb').write(content)
-                                    else:
-                                        content = data.decode(
-                                            source_encoding["encoding"]
-                                        ).encode('utf-8').strip()
-                                        codecs.open(each_full_name,
-                                                    'wb').write(content)
+                                        content = content.encode("utf-8").strip()
+                                        codecs.open(each_full_name, "wb").write(
+                                            str(content)
+                                        )
+                                    elif se:
+                                        content = (
+                                            data.decode(se).encode("utf-8").strip()
+                                        )
+                                        codecs.open(each_full_name, "wb").write(
+                                            str(content)
+                                        )
 
-                        with io.open(each_full_name, mode="r",
-                                     encoding="utf-8") as file_load:
+                        with io.open(
+                            each_full_name, mode="r", encoding="utf-8"
+                        ) as file_load:
                             file_size = os.path.getsize(each_full_name)
                             content = file_load.read()
                             word_num = len(content.strip())
                             item_dict = {
                                 "name": each_name,
                                 "size": size_convert(file_size),
-                                "count": word_num
+                                "count": word_num,
                             }
                             res_dict["txt"].append(item_dict)
 
@@ -103,7 +108,7 @@ def is_empty_dir(dir_path):
         bool: 为空时返回 True
     """
     for name in os.listdir(dir_path):
-        if is_txt_ext(name):
+        if is_txt_ext(name) or os.path.isdir(os.path.join(dir_path, name)):
             return False
     return True
 
@@ -118,16 +123,20 @@ def read_json_file(path, default={}):
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser(description="水文仓库文章索引器")
-    ap.add_argument("-n",
-                    "--new",
-                    action="store_true",
-                    required=False,
-                    help="重建索引文件(会覆盖已存在的索引文件)")
-    ap.add_argument("-c",
-                    "--convert",
-                    action="store_true",
-                    required=False,
-                    help="索引过程中同时将文本文件编码转为 utf-8")
+    ap.add_argument(
+        "-n",
+        "--new",
+        action="store_true",
+        required=False,
+        help="重建索引文件(会覆盖已存在的索引文件)",
+    )
+    ap.add_argument(
+        "-c",
+        "--convert",
+        action="store_true",
+        required=False,
+        help="索引过程中同时将文本文件编码转为 utf-8",
+    )
 
     args = vars(ap.parse_args())
     xlog = Logger("DEBUG", "log")
